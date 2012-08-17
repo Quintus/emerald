bac54d09f7ede0570e19c7abcd04b076625706b2
Quintus <quintus@quintilianus.eu>
2012-08-17 11:52:17 +0200

    Improve the JavaScript search.
    It now searches all three indices and adds markers around the
    matched text. After a search has finished, some cleanup is now
    also done.

--- data/javascripts/emerald.js
+++ data/javascripts/emerald.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 
-    // The function for showing/hiding the complete file list
+    /**
+     * Hides/shows the complete list of files in the
+     * file index. Also changes the toggle link’s text
+     * accodingly.
+     */
     $("a#toggle-all-files").click(function(event){
         $("li.code-file").toggle();
 
@@ -12,7 +16,10 @@ $(document).ready(function(){
         return false;
     });
 
-    // The function for showing/hiding a method’s sourcecode.
+    /**
+     * Hides/showes the sourcecode of a method, changing
+     * the toggle link’s text accordingly.
+     */
     $("p.show-source a").click(function(event){
         $(this).parent().next().toggle("slow");
 
@@ -24,8 +31,11 @@ $(document).ready(function(){
         return false;
     });
 
-    // The function that displays the ¶ sign when hovering
-    // over a heading (except level-1 headings).
+    /**
+     * Shows/hides the ¶ anchor link signs next to
+     * headings when hovering with the mouse over
+     * a heading.
+     */
     $("h2, h3, h4, h5, h6").hover(
         function(event){
             $(this).append('<a class="para" href="' + window.location.pathname + '#' + $(this).attr('id') + '">&nbsp;¶</a>');
@@ -35,14 +45,69 @@ $(document).ready(function(){
         }
     );
 
+    /**
+     * This code handles the search field, showing and hiding
+     * matching/non-matching elements. When the search has ended,
+     * i.e. the search field is empty again, restores the vanilla
+     * status including hid of the code files in the file index.
+     * A small problem with this is, if the file index had been
+     * expanded by the user, it will now be collapsed again. However,
+     * this should not be a major problem.
+     *
+     * The matches of the search expression are surrounded with a
+     * <span class="search-result"> element, which is completely
+     * removed when the search has ended.
+     */
     $("form#search p input").keyup(function(event){
-        term = new RegExp($(this).val(), "i");
+        // These are the lists we are going to iterate over
+        // when searching for the user’s query.
+        var targets = $("div#method-index ul li, div#class-index ul li, div#file-index ul li");
 
-        // Method index
-        $("div#method-index ul li, div#class-index ul li").each(function(index){
-            var result = $(this).text().match(term);
-            if (result)
+        // If the search field is empty, consider the search to
+        // have been ended; restore original status.
+        if ($(this).val().length == 0){
+            // Show all elements (except for code file names, which
+            // are hidden by default and shall return thereto).
+            targets.each(function(index){
+                // Remove any <span> element relicts
+                var anchor = $(this).find("a");
+                anchor.text(anchor.text()); // The text() getter works recursively, removing child structure nodes!
+
+                // Show everything except for code files in the file index
+                if ($(this).hasClass("code-file"))
+                    $(this).hide();
+                else
+                    $(this).show();
+            });
+
+            // Ensure the "Show all" link for the file index shows
+            // "Show all" now as we’ve hidden the code files above.
+            $("a#toggle-all-files").text("Show all").show();
+
+            // No need for further investigation
+            return;
+        }
+
+        // Convert the user input into a Regular Expression.
+        var term = new RegExp($(this).val(), "i");
+
+        // Hide the "Show all/Hide extra" link when searching
+        $("a#toggle-all-files").hide();
+
+        // Iterate through all the lists and mark+show all matching
+        // links. Hide those links that do not match.
+        targets.each(function(index){
+            var anchor = $(this).find("a");
+
+            // Exclude the "Show all/Hide extra" link in the file
+            // index from the search.
+            if (anchor.attr("id") == "toggle-all-files")
+                return true
+
+            if (term.test(anchor.text())){
+                anchor.html(anchor.text().replace(term, "<span class=\"search-result\">$&</span>"));
                 $(this).show();
+            }
             else
                 $(this).hide();
         });
